6b192f115d1c4a7791996efdd639598bd7644c87
zeelvaghasiya19 <117510949+zeelvaghasiya19@users.noreply.github.com>
2025-10-10 11:07:51 +0000

    week1

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,36 @@
+console.log("this is my first programe")
+console.log("wellcome john your monthly salary is 500000")
+
+
+const num1 = 5;
+const num2 = 3;
+//add two number
+const sum = num1 + num2;
+//display the sum
+console.log('the sum of' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+
+
+// app.js or index.js
+const prompt = require('prompt-sync')(); // This line is essential
+console.log("starting")
+const name = prompt('Enter your name: ');
+console.log(`Hello, ${name}`);
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number = parseInt(prompt("Enter a number: "));
+// check if number is greater than 0
+if (number > 0) {
+console.log("The number is positive");
+}
+// check if number is 0
+else if (number == 0) {
+console.log("The number is zero");
+}
+// if number is less than 0
+else {
+console.log("The number is negative");
+}
+
+
 
